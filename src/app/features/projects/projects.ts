fffb456c82db44f80bfdc8d64372aa9bf9f99283
Lei Lorenz Xavier <lorins.sabyir@gmail.com>
2026-05-08 10:25:27 +0800

    feat: :sparkles: implement project-card component; refactor projects section to use new component and update styles

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { ProjectCard } from "../../shared/project-card/project-card";
 
 @Component({
   selector: 'app-projects',
-  imports: [NgFor],
+  imports: [ProjectCard],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
@@ -13,18 +13,21 @@ export class Projects {
       title: 'IoT Control Hub',
       description:
         'A dark-themed real-time dashboard built with Angular that communicates with backend services to display live device states and metrics.',
+      link: "https://github.com/LorinsSabyir",
       tags: ['Angular', 'Python', 'WebSockets'],
     },
     {
       title: 'E-Commerce Matrix',
       description:
         'A headless WordPress implementation featuring a highly optimized frontend interface and a modular backend content model.',
+      link: "https://github.com/LorinsSabyir",
       tags: ['WordPress', 'PHP', 'UI/UX'],
     },
     {
       title: 'Network Packet Analyzer',
       description:
         'A Java-powered tool with a connected web interface that intercepts, visualizes, and breaks down network traffic for analysis.',
+      link: "https://github.com/LorinsSabyir",
       tags: ['Java', 'Networking', 'Frontend'],
     },
   ];
